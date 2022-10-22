64c1264eceb10cbfe7d3fa4a3cd0fc6005299ca3
Billy Fletcher <Billyfletcher99@gmail.com>
2022-10-22 19:28:51 -0400

    having strange issue of 10k commits needed lol

--- Develop/server/server.js
+++ Develop/server/server.js
@@ -4,26 +4,12 @@ const db = require('./config/connection');
 const routes = require('./routes');
 const { ApolloServer} = require('apollo-server-express');
 
-//apollo server
-const server = new ApolloServer({
-  typeDefs,
-  resolvers,
-  context: authMiddleware
-});
-
-server.applyMiddleware({app});
-
-const {typeDefs,resolvers} = require('./schemas');
-
-const {authMiddleware} = require('./utils/auth');
-
 const app = express();
 const PORT = process.env.PORT || 3002;
 
 app.use(express.urlencoded({extended: true}));
 
 app.use(express.json());
-// if we're in production, serve client/build as static assets
 
 if (process.env.NODE_ENV === 'production') 
 {app.use(express.static(path.join(__dirname, '../client/build')));}
@@ -31,6 +17,6 @@ if (process.env.NODE_ENV === 'production')
 app.get('*', (req, res) => {res.sendFile(path.join(__dirname,'../client/build/index.html'));});
 
 app.use(routes);
-db.once('open', () => {app.listen(PORT, () => {
-    console.log(`API server running on port ${PORT}!`);
-    console.log(`Use GraphQL at http://localhost:${PORT}${server.graphqlPath}`);});});
+db.once('open', () => {
+  app.listen(PORT, () => console.log(`You are on localhost:${PORT}`));
+});
