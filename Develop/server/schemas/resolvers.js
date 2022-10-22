e65084c3674f88344c131bf1309d77a305c81346
Billy Fletcher <Billyfletcher99@gmail.com>
2022-10-22 19:31:29 -0400

    We're gonna try to stage and commit all 10k lol

--- Develop/server/schemas/resolvers.js
+++ Develop/server/schemas/resolvers.js
@@ -1,6 +1,7 @@
-const { User, Book } = require('../models');
 const { AuthenticationError } = require('apollo-server-express');
 const { signToken } = require('../utils/auth');
+const { User, Book } = require('../models');
+
 
 const resolvers = {
 	Query: {
